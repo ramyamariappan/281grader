6306b24300d02cb0e31168df0b35a0a890d506d1
ramyamariappan <ramya.mariappan@sjsu.edu>
2017-05-07 00:00:21 -0700

    fixing parser paths

--- grader/config/parsePaths.js
+++ grader/config/parsePaths.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'tenantA' : 'java -jar umlparserB.jar ./unzipped ./unzipped/uml.png',
-  'tenantB' : 'java -jar umlparser.jar class ./unzipped uml',
+  'tenantB' : 'java -jar umlparserB.jar ./unzipped ./unzipped/uml.png',
+  'tenantA' : 'java -jar umlparser.jar class ./unzipped uml',
 }
